94c56c7a289b74cf38a2553c0aeb5b0c1a047955
Envyiwnl <parvezmusharf@gmail.com>
2026-09-10 19:03:08 +0530

    Configure Firebase credentials for deployment

--- server/src/config/firebaseAdmin.js
+++ server/src/config/firebaseAdmin.js
@@ -2,12 +2,11 @@ import { readFileSync } from "node:fs";
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-const serviceAccount = JSON.parse(
-  readFileSync(
-    new URL("../../secrets/firebase-service-account.json", import.meta.url),
-    "utf-8",
-  ),
-);
+const serviceAccountPath =
+  process.env.FIREBASE_SERVICE_ACCOUNT_PATH ||
+  new URL("../../secrets/firebase-service-account.json", import.meta.url);
+
+const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf-8"));
 
 const adminApp =
   getApps().length > 0
