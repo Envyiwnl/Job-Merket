ec026f2c97731e43418b30b00864d046edb7dafe
Envyiwnl <parvezmusharf@gmail.com>
2026-09-10 20:26:56 +0530

    Fix component import casing

--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -1,6 +1,6 @@
 import CTA from "../components/CTA";
 import FeaturedJobs from "../components/FeaturedJobs";
-import Features from "../components/features";
+import Features from "../components/Features";
 import Hero from "../components/Hero";
 import HowItWorks from "../components/HowItWorks";
 import PopularCategories from "../components/PopularCategories";
